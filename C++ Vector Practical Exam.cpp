1e0d19aa41a9db6fe50748fcb9c5731619d8d4c6
Saif Mohamed <bvdwsuxci@gmail.com>
2026-09-24 20:00:49 +0300

    Add C++ vector practical exam template

---
+++ C++ Vector Practical Exam.cpp
@@ -0,0 +1,269 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    
+   /* int n;
+    cout<<"enter the size of the vector : ";
+    cin >> n;
+	int max = 0;
+	vector <int> v;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "enter the value of element " << endl;
+		cin >> v[i];
+		if(max < v[i])
+		{
+			max = v[i];
+		}
+	}
+	cout << "the maximum element in the vector is : " << max << endl;*/
+
+	
+
+	/*int n;
+	cout << "enter the size of the vector : ";
+	cin >> n;
+	vector <int> v(n);
+	int z;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cout << "enter the value of element " << endl;
+		cin >> x;
+		v[i] = x;
+	}
+	cin >> z;
+	for (int i = 0; i < n; i++) {
+		if (v[i] == z) {
+			cout << "found.";
+			break;
+		}
+		else {
+			cout << "not found.";
+			break;
+		}
+	}*/
+
+
+
+
+
+
+	/*int n;
+	cout << "enter the size of the vector : ";
+	cin >> n;
+	vector <int> v(n);
+	for (int i = 0; i < n; i++) {
+		cout << "enter the value of element " << endl;
+		cin >> v[i];
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		cout << v[i] << endl;
+	}*/
+
+
+
+
+
+	
+
+
+	/*int n;
+	cout << "enter the size of the vector : ";
+	cin >> n;
+	vector <int> v(n);
+	
+	for (int i = 0; i < n; i++) {
+		cout << "enter the value of element " << endl;
+		cin >> v[i];
+	}int x;
+
+	cout << "enter the element to count its occurrences : ";
+	cin >> x;
+	int count = 0;
+		for(int i = 0; i < v.size(); i++){
+		
+		
+		
+		if (v[i] == x)
+		{
+			count++;
+		}
+	}
+	cout << "the number of occurrences of the element is : " << count << endl;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//Question 12 - Palindrome
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int n; 
+	//cout << "enter the size of the vector : ";
+	//cin >> n;
+	//vector<int> vec(n);
+	//for (int i = 0; i < n; i++) {
+	//	cout << "enter the value of element: ";
+	//	cin >> vec[i];
+	//}
+	//int maxx = vec[0];
+	//int second = vec[0];
+
+	//int x;
+	//for (int i = 1; i < vec.size(); i++) {
+	//	if (vec[i] > maxx) {
+	//		second = maxx;
+	//		maxx = vec[i];
+	//	}
+	//	else if (vec[i] > second && vec[i] != maxx) {
+	//		second = vec[i];
+	//	}
+	//}
+	//
+	//cout << "its last largest: " << second << endl;
+
+
+	
+
+
+
+	/*int n;
+	cout << "enter the size of the vector : ";
+	cin >> n;
+	int zero = 0;
+	vector <int> v(n);
+	for (int i = 0; i < n; i++) {
+		cout << "enter the value of element " << endl;
+		cin >> v[i];
+	}
+	for (int i = 0; i < n; i++) {
+		if (v[i] == 0) {
+			zero++;
+		}
+		
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		if (v[i] != 0) {
+			cout << v[i] << endl;
+		}
+	}
+	for (int i = 0; i < zero; i++) {
+		cout << 0 << endl;
+	}*/
+	
+
+
+
+
+
+
+
+
+	//Question 15 - Remove Duplicates
+
+
+
+
+
+
+
+
+
+
+	//int n;
+	//cout << "enter the size of the vector : ";
+	//cin >> n;
+	//vector <int> v(n);
+	//for (int i = 0; i < n; i++) {
+	//	cout << "enter the value of element " << endl;
+	//	cin >> v[i];
+	//	
+	//}
+	//int n2;
+	//cout << "enter the size of the vector : ";
+	//cin >> n2;
+	//cout << " ";
+	//vector <int> v2(n2);
+	//for (int i = 0; i < n2; i++) {
+	//	cout << "enter the value of element " << endl;
+	//	cin >> v2[i];
+	//	
+	//}
+	//vector<int> result(n + n2);
+
+	//for (int i = 0; i < n; i++) {
+	//	result[i] = v[i];
+
+	//}
+	//for (int i = 0; i < n2; i++) {
+	//	result[n + i] = v2[i];
+	//}
+	//for (int i = 0; i < result.size(); i++) {
+	//	cout << result[i] << " ";
+
+	//}
+	
+
+
+
+	//Question 17 - Common Elements
+
+
+
+
+
+
+
+
+
+	//Question 18 - Rotate Right
+
+
+
+
+
+
+
+	//Question 19 - Find Missing Number
+
+
+
+
+
+
+
+
+
+	//Question 20 - Maximum Subarray Sum
+
+
+
+
+
+
+}
